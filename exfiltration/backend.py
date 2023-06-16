897d88beae317b9b2fe03027722ced68a568d156
Nicicalu <caluori.nici@gmail.com>
2023-06-16 17:33:26 +0200

    log line

--- exfiltration/backend.py
+++ exfiltration/backend.py
@@ -24,4 +24,4 @@ while True:
             # Do something with the extracted information
             print(f"New query arrived from {ip_address} for {domain_name} ({query_type}/{query_class})")
         else:
-            print("No match")
+            print(f"No match {line.decode()}")
