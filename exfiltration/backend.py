7b1834de048f9a068670ecb28865ea374d1c7f6e
Nicicalu <caluori.nici@gmail.com>
2023-06-16 17:37:16 +0200

    Encoding

--- exfiltration/backend.py
+++ exfiltration/backend.py
@@ -1,10 +1,15 @@
 import subprocess
 import re
+import sys
 
 logfile = "../bind/log/query.log"
 
 # Define a regular expression pattern to match the log line
 pattern = r"queries: client @\S+ (\d+\.\d+\.\d+\.\d+)#\d+ \((\S+)\): query: (\S+) (\S+) (\S+) -\S+\(\d+\)D \(\S+\)"
+
+# Print out the encoding of the log file and the regular expression pattern
+print(f"Log file encoding: {sys.getdefaultencoding()}")
+print(f"Pattern encoding: {sys.getdefaultencoding()}")
 
 # Start tailing the log file
 p = subprocess.Popen(['tail', '-F', logfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -13,8 +18,11 @@ p = subprocess.Popen(['tail', '-F', logfile], stdout=subprocess.PIPE, stderr=sub
 while True:
     line = p.stdout.readline()
     if line:
+        # Convert the line to a string using the correct encoding
+        line_str = line.decode(sys.getdefaultencoding()).strip()
+
         # Use the regular expression pattern to extract the relevant information from the log line
-        match = re.match(pattern, "16-Jun-2023 15:28:08.026 queries: client @0x7f3d18004548 193.135.143.35#56620 (test46.dnshell.programm.zip): query: test46.dnshell.programm.zip IN AAAA -E(0)D (192.168.64.2)")
+        match = re.match(pattern, line_str)
         if match:
             ip_address = match.group(1)
             domain_name = match.group(2)
@@ -24,5 +32,5 @@ while True:
             # Do something with the extracted information
             print(f"New query arrived from {ip_address} for {domain_name} ({query_type}/{query_class})")
         else:
-            print(f"No match {line.decode()}")
+            print(f"No match {line_str}")
             print(f"Pattern {pattern}")
